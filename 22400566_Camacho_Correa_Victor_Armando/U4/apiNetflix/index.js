4264b20387ed1a1759904552f5a987ae9d932d43
viarcamachoco <viarcamachoco@ittepic.edu.mx>
2026-07-21 09:22:20 -0700

    NetflixApi1.1

--- 22400566_Camacho_Correa_Victor_Armando/U4/apiNetflix/index.js
+++ 22400566_Camacho_Correa_Victor_Armando/U4/apiNetflix/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-
+const cors = require('cors');
 const app = express();
 const PORT = 4000;
 
