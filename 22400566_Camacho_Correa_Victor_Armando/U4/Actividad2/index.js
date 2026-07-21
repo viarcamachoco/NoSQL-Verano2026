d540c7dade7c8d70b029de0f5451a02e0c40d7df
viarcamachoco <viarcamachoco@ittepic.edu.mx>
2026-07-21 08:23:21 -0700

    Mongoose

--- 22400566_Camacho_Correa_Victor_Armando/U4/Actividad2/index.js
+++ 22400566_Camacho_Correa_Victor_Armando/U4/Actividad2/index.js
@@ -1,79 +1,185 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const PORT = 4000;
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const nombre = "Victor";
+app.use(express.json());
+
 app.use(morgan('dev'));
 
-app.get('/par/:numero', (req, res) => {
-  if (req.params.numero % 2 === 0) {
-    res.send('El numero es par');
-  } else {
-    res.send('El número no es par');
-  }
+mongoose.connect('mongodb://127.0.0.1:27017/escuela').then(() => {
+    console.log("Conectado a la base de datos");
+}).catch((error) => {
+    console.log("Error al conectar a la base de datos: ", error);
 });
 
-app.get('/edad/:edad', (req, res) => {
-  if (req.params.edad >= 18) {
-    res.send('Eres mayor de edad');
-  } else {
-    res.send('Eres menor de edad');
-  }
+const alumnoSchema = new mongoose.Schema({
+    nombre: {type: String, required: true, trim: true},
+    carrera: {type: String, required: true, trim: true},
+    semestre: {type: Number, required: true,min: 1}
+},
+{
+    timestamps: true
+}
+);
+
+const Alumno = mongoose.model('Alumno', alumnoSchema, 'alumnos');
+
+app.get("/alumnos",async (req,res) =>{
+   try{
+        const alumnos = await Alumno.find();
+        res.json(alumnos);
+} catch (error) {
+    res.status(500).json({mensaje: "Error interno del servidor", error: error});
+}
 });
 
-app.get('/calculadora/:operacion/:a/:b', (req, res) => {
-  const a = Number(req.params.a);
-  const b = Number(req.params.b);
-  let resultado;
+app.get("/alumnos/:id", async (req,res) =>{
+    try{
+    const id = Number(req.params.id);
+    const alumno = await Alumno.findById(id);
+    if(!alumno){
+        return res.status(404).json({mensaje: "Alumno no encontrado"});
+    }
+    res.json(alumno);
+} catch (error) {
+    res.status(500).json({mensaje: "Error interno del servidor", error: error});    
+}});
 
-  switch (req.params.operacion) {
-    case 'sumar':
-      resultado = a + b;
-      break;
-    case 'restar':
-      resultado = a - b;
-      break;
-    case 'multiplicar':
-      resultado = a * b;
-      break;
-    case 'dividir':
-      resultado = a / b;
-      break;
-    default:
-      res.status(400).send('Operación no válida');
-      return;
-  }
+app.post("/alumnos", async (req,res) =>{
+    try{
+    const {nombre, carrera, semestre} = req.body;
+    if(!nombre || !carrera || !semestre){
+        return res.status(400).json({mensaje: "Faltan datos del alumno"});
+    }
+    const nuevoAlumno = new Alumno({
+        nombre,
+        carrera,
+        semestre
+    });
+    const alumnoGuardado = await nuevoAlumno.save();
+    res.json({mensaje: "Alumno registrado correctamente", alumno: nuevoAlumno});
+} catch (error) {
+    res.status(500).json({mensaje: "Error interno del servidor", error: error});    
+}});
 
-  res.send(`El resultado de ${req.params.operacion} ${a} y ${b} es: ${resultado}`);
+app.put("/alumnos/:id", async (req,res) =>{
+    try{
+    const id = Number(req.params.id);
+    const {nombre, carrera, semestre} = req.body;
+
+    if(!nombre || !carrera || !semestre){
+        return res.status(400).json({mensaje: "Faltan datos del alumno"});
+    }
+    const indice = alumnos.findIndex(alumno => alumno.id === id);
+    if(indice === -1){
+        return res.status(400).json({
+            mensaje: "Alumno no encontrado"
+        });
+    }
+    alumnos[indice] = {
+        id: id,
+        nombre: nombre,
+        carrera: carrera,
+        semestre: semestre
+    }
+    const alumnoActualizado = await Alumno.findByIdAndUpdate(id, {
+        nombre,
+        carrera,
+        semestre
+    }, {new: true});
+
+    if(!alumnoActualizado){
+        return res.status(404).json({mensaje: "Alumno no encontrado"});
+    }
+
+    res.json({
+        mensaje: "Alumno actualizado correctamente",
+        alumno: alumnoActualizado
+    });
+    } catch (error) {
+        res.status(500).json({mensaje: "Error interno del servidor", error: error});
+    }
+});
+    
+app.delete("/alumnos/:id", async (req,res) =>{
+    try{
+    const id = Number(req.params.id);
+    const alumnoEliminado = await Alumno.findByIdAndDelete(id);
+    if(!alumnoEliminado){
+        return res.status(404).json({mensaje: "Alumno no encontrado"});
+    }
+    res.json({mensaje: "Alumno eliminado correctamente", alumno: alumnoEliminado});
+} catch (error) {
+    res.status(500).json({mensaje: "Error interno del servidor", error: error});
+}
 });
 
-app.get('/tabla/:numero', (req, res) => {
-  const numero = Number(req.params.numero);
-  let tabla = '';
 
-  for (let i = 1; i <= 10; i++) {
-    tabla += `${numero} x ${i} = ${numero * i}<br>`;
-  }
-
-  res.send(`Tabla de multiplicar del ${numero}:<br>${tabla}`);
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
 });
 
-app.get('/calificacion/:calificacion', (req, res) => {
-  const calificacion = Number(req.params.calificacion);
-  let mensaje;
+app.get("/mensaje",(req, res) => {
+  res.send("Mensaje desde express");
+});
 
-  if (calificacion >= 90) {
-    mensaje = 'Excelente';
-  } else if (calificacion >= 80) {
-    mensaje = 'Muy Bien';
-  } else if (calificacion >= 70) {
-    mensaje = 'Aprobado';
-  } else {
-    mensaje = 'Reprobado';
-  }
+app.get("/pagina", (req, res)=>{
+    res.send(`
+        <style>
+            .p1{
+                color: red;
+                background: blue;
+            }
+        </style>
+            <h1> Mi pagina web</h1>
+            <p class="p1">¡Hola ${nombre}!</p>
+        `)
+});
 
-  res.send(`Calificación: ${calificacion} - ${mensaje}`);
+app.get("/alumno", (req, res) =>{
+    res.json({
+        nombre: "Victor",
+        carrera: "ISC",
+        semestre: 8
+    })
+});
+
+app.get("/materias", (req, res) =>{
+    res.json([
+        {
+            nombre: "NoSQL",
+            hora: "8:00-11:00"
+        },
+        {
+            nombre: "Programacion Web",
+            hora: "14:00-17:00"
+        },
+            ])
+});
+
+app.get("/mensaje/:nombre", (req, res) =>{
+    res.send(`Hola ${req.params.nombre}`)
+})
+
+app.get("/suma/:a/:b", (req, res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+    res.send(`La suma de ${a} y ${b} es: ${a+b}`);
+});
+
+app.get("/multiplicacion/:a/:b", (req, res)=>{
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+    res.send(`La multiplicacion de ${a} y ${b} es: ${a*b}`);
+});
+
+app.get("/aleatorio", (req, res)=>{
+    const numero = Math.floor(Math.random()*100)+1;
+    res.send(`Número aleatorio: ${numero}`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log("Servidor iniciado en: http://localhost:"+PORT);
 });
